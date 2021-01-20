d02c9236579f3ec8781aa11c4964cb99435e3930
santosl <luis.santos@faro.com>
2021-01-20 17:44:40 +0000

    initial commit

---
+++ graph.drawer/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Flow.Wpf;
+using graph.drawer.ViewModels;
+using graph.drawer.Views;
+
+namespace graph.drawer
+{
+
+    public partial class App
+    {
+
+
+        public App()
+        {
+            ThisAssembly = typeof(App).Assembly;
+            Host = new CaliburnHost<MainViewModel>(this);
+        }
+
+        private Assembly ThisAssembly { get; }
+        private CaliburnHost<MainViewModel> Host { get; }
+
+    }
+
+}
---
+++ json.parser/JsonReader.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace json.parser
+{
+
+    public class JsonReader
+    {
+
+        public IEnumerable<Item> Read(string json)
+        {
+            return new Item[] {
+                    new Item() {Kind = Kind.Service},
+                    new Item() {Kind = Kind.Service},
+                    new Item() {Kind = Kind.ExternalService},
+                    new Item() {Kind = Kind.Deployment},
+                    new Item() {Kind = Kind.Ingress},
+                    new Item() {Kind = Kind.Job},
+                    new Item() {Kind = Kind.Deployment},
+            };
+        } // TODO
+
+    }
+
+
+    public class Item
+    {
+
+        public Kind Kind { get; set; }
+
+    }
+
+
+    public enum Kind
+    {
+
+        Service,
+        ExternalService,
+        Deployment,
+        Ingress,
+        Job
+
+    }
+
+
+    public abstract class Parser<T>
+    {
+
+        private readonly JsonReader _reader;
+
+        public T Parse(string json) => Order(_reader.Read(json));
+
+        protected abstract T Order(IEnumerable<Item> items);
+
+        protected Parser(JsonReader reader)
+        {
+            _reader = reader;
+        }
+
+    }
+
+
+    public class ListParser : Parser<IEnumerable<Item>>
+    {
+
+        public ListParser(JsonReader reader) : base(reader) { }
+
+        protected override IEnumerable<Item> Order(IEnumerable<Item> items) => items; //TODO
+
+    }
+
+
+    public class TreeParser : Parser<IEnumerable<Item>>
+    {
+
+        public TreeParser(JsonReader reader) : base(reader) { }
+
+        protected override IEnumerable<Item> Order(IEnumerable<Item> items) => items.Reverse(); //TODO
+
+    }
+
+
+    public class ParsersServiceCollection : ServiceCollection
+    {
+
+        public ParsersServiceCollection()
+        {
+            this.AddSingleton(typeof(ILogger), sp => sp.GetService<ILoggerFactory>().CreateLogger(typeof(ParsersServiceCollection)))
+                .AddSingleton(typeof(ILogger<>), typeof(Logger<>))
+                .AddSingleton<ILoggerFactory, LoggerFactory>()
+                .AddSingleton<JsonReader>()
+                .AddSingleton<ListParser>()
+                .AddSingleton<TreeParser>();
+        }
+
+    }
+
+}
