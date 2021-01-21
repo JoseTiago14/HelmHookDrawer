536aa64d699d6d132d2e682317fa9f914d272d6e
Jose Tiago Pereira de Carvalho <Jose.Pereira@faro.com>
2021-01-21 19:38:12 +0000

    Added tooltip to reource view and icon to app

--- graph.drawer/App.xaml.cs
+++ graph.drawer/App.xaml.cs
@@ -1,20 +1,31 @@
-﻿using System.Reflection;
+﻿using System.Net.Mime;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Navigation;
 using Flow.Wpf;
 using graph.drawer.ViewModels;
-using graph.drawer.Views;
 
 namespace graph.drawer
 {
 
     public partial class App
     {
-
         public App()
         {
             ThisAssembly = typeof(App).Assembly;
             Host = new CaliburnHost<MainViewModel>(this,
                                                    ("Flow", ThisAssembly));
+            
         }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (Current.MainWindow != null)
+                Current.MainWindow.Title = "Hook Drawer";
+        }
+
 
         private Assembly ThisAssembly { get; }
         private CaliburnHost<MainViewModel> Host { get; }
